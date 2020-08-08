34248bd68c3e866957ae9fc1bf21280a4000c4c4
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-08-09 01:32:13 +0530

    figured min temp for two whethe stations

--- min-temperatures.py
+++ min-temperatures.py
@@ -1,6 +1,6 @@
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setMaster("local").setAppName("MinTemperatures")
+conf = SparkConf().setMaster('local').setAppName('Mintemperature')
 sc = SparkContext(conf = conf)
 
 def parseLine(line):
@@ -9,13 +9,17 @@ def parseLine(line):
     entryType = fields[2]
     temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0
     return (stationID, entryType, temperature)
+  
 
-lines = sc.textFile("file:///SparkCourse/1800.csv")
+#create a rdd
+lines = sc.textFile('file:////home/yudi/workspace/Spark-practice/1800.csv')
 parsedLines = lines.map(parseLine)
-minTemps = parsedLines.filter(lambda x: "TMIN" in x[1])
+minTemps = parsedLines.filter(lambda x: 'TMIN' in x[1])
 stationTemps = minTemps.map(lambda x: (x[0], x[2]))
-minTemps = stationTemps.reduceByKey(lambda x, y: min(x,y))
-results = minTemps.collect();
+minTemps = stationTemps.reduceByKey(lambda x, y: min(x, y))
+results = minTemps.collect()
+
 
 for result in results:
     print(result[0] + "\t{:.2f}F".format(result[1]))
+
