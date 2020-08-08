f0fb93d843990cfaaeb16dc40f7286b41d78e494
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-08-09 01:43:57 +0530

    finding number of words in the file\

--- word-count.py
+++ word-count.py
@@ -1,13 +1,16 @@
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setMaster("local").setAppName("WordCount")
+conf = SparkConf().setMaster('local').setAppName('WordCount')
+
 sc = SparkContext(conf = conf)
 
-input = sc.textFile("file:///sparkcourse/book.txt")
+input = sc.textFile('file:////home/yudi/workspace/Spark-practice/Book')
 words = input.flatMap(lambda x: x.split())
+
+#create a dictionary which contains the number of each occurance for every word...
 wordCounts = words.countByValue()
 
 for word, count in wordCounts.items():
     cleanWord = word.encode('ascii', 'ignore')
     if (cleanWord):
-        print(cleanWord.decode() + " " + str(count))
+        print(cleanWord, count)
