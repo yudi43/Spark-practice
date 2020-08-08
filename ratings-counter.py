795112b724030267038675d80b851cd95dfa0700
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-08-08 18:50:54 +0530

    added fake friends

--- ratings-counter.py
+++ ratings-counter.py
@@ -11,7 +11,4 @@ sc = SparkContext(conf = conf)
 lines = sc.textFile("file:////home/yudi/workspace/Spark-practice/ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
-
-sortedResults = collections.OrderedDict(sorted(result.items()))
-for key, value in sortedResults.items():
     print("%s %i" % (key, value))
