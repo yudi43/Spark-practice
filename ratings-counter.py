621d7f68ac86ca26aed0db129113e35580e1a525
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-08-08 17:19:51 +0530

    refactoring

--- ratings-counter.py
+++ ratings-counter.py
@@ -1,9 +1,13 @@
 from pyspark import SparkConf, SparkContext
 import collections
 
+
+#writing local means not running on cluster
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
+
+#the most common way of creating a rdd is using textFile
 lines = sc.textFile("file:////home/yudi/workspace/Spark-practice/ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
